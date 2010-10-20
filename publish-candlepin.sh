7dd4f6304ea64726dd498d7568ced60b82db19a9
jesus m. rodriguez <jesusr@redhat.com>
2010-10-20 15:33:41 -0400

    remove commented lines

--- publish-candlepin.sh
+++ publish-candlepin.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#if [ "$1" == "" ] ; then
-#   echo "specify a version as the first arg"
-#   exit
-#fi
 
 rm -rf /tmp/candlepin
 mkdir -p /tmp/candlepin
@@ -10,5 +6,4 @@ cd /tmp/candlepin/
 brew download-build --latestfrom candlepin-1-rhel5-candidate candlepin
 rm -f candlepin*src.rpm
 createrepo --checksum sha -d .
-echo 'rsync -avz --delete --no-p --no-g /tmp/candlepin/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/'
-#rsync -avz --delete --no-p --no-g /tmp/candlepin/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/
+rsync -avz --delete --no-p --no-g /tmp/candlepin/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/
