eadb62feda90bf8313a6e9733c17d04843eda5c4
jesus m. rodriguez <jesusr@redhat.com>
2011-03-28 22:28:18 -0400

    add sync scripts

---
+++ sync-fedora.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    echo "Supply a Candlepin version."
+    exit 1;
+fi
+
+rm -rf /tmp/candlepin/$1/Fedora/
+mkdir -p /tmp/candlepin/$1/Fedora/13/
+cp /tmp/cp-fedora-13-x86_64/* /tmp/candlepin/$1/Fedora/13/
+cd /tmp/candlepin/$1/Fedora/13/
+createrepo -d .
+
+mkdir -p /tmp/candlepin/$1/Fedora/14/
+cp /tmp/cp-fedora-14-x86_64/* /tmp/candlepin/$1/Fedora/14/
+cd /tmp/candlepin/$1/Fedora/14/
+createrepo -d .
+
+#rsync -avz --delete --no-p --no-g /tmp/candlepin/$1/Fedora/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/$1/Fedora/
+mkdir -p /tmp/dept/yum/candlepin/$1/Fedora/
+rsync -avz --delete --no-p --no-g /tmp/candlepin/$1/Fedora/ /tmp/dept/yum/candlepin/$1/Fedora/
---
+++ sync-rhel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    echo "Supply a Candlepin version."
+    exit 1;
+fi
+
+rm -rf /tmp/candlepin/$1/RHEL/
+
+mkdir -p /tmp/candlepin/$1/RHEL/5/
+cd /tmp/candlepin/$1/RHEL/5
+brew download-build --latestfrom candlepin-1-rhel5-candidate candlepin
+rm -f candlepin*src.rpm
+createrepo --checksum sha -d .
+
+mkdir -p /tmp/candlepin/$1/RHEL/6/
+cd /tmp/candlepin/$1/RHEL/6
+brew download-build --latestfrom candlepin-1-rhel6-candidate candlepin
+rm -f candlepin*src.rpm
+createrepo --checksum sha -d .
+
+#rsync -avz --delete --no-p --no-g /tmp/candlepin/$1/RHEL/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/$1/RHEL/
+mkdir -p /tmp/dept/yum/candlepin/$1/RHEL/
+rsync -avz --delete --no-p --no-g /tmp/candlepin/$1/RHEL/ /tmp/dept/yum/candlepin/$1/RHEL/
