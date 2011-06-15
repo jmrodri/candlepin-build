0e6a16dc0d287f8ecdd3d41fd5fc0da0e1959e93
jesus m. rodriguez <jesusr@redhat.com>
2011-06-15 14:57:43 -0400

    script to prep repos for fedora syncing

---
+++ sync-open.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    echo "Supply a Candlepin version i.e. 0.3, 0.4."
+    exit 1;
+fi
+
+function create_repo {
+    pushd $2
+    if [ "$1" == "epel" ] ; then
+        /usr/bin/createrepo --checksum sha -d .
+    else
+        /usr/bin/createrepo -d .
+    fi
+    popd
+}
+
+function prep_epel_repo {
+    cpver=$1
+    epelver=$2
+    mkdir -p /tmp/candlepin/$cpver/epel-$epelver/{i386,SRPMS,x86_64}
+    cd /tmp/candlepin/$cpver/epel-$epelver
+    brew download-build --latestfrom candlepin-1-rhel$epelver-candidate candlepin
+    mv candlepin*src.rpm SRPMS/
+    cp candlepin*.rpm i386/
+    cp candlepin*.rpm x86_64/
+    rm -f candlepin*.rpm
+    create_repo epel i386
+    create_repo epel x86_64
+    create_repo epel SRPMS
+}
+
+function prep_fedora_repo {
+    cpver=$1
+    fedoraver=$2
+    mkdir -p /tmp/candlepin/$cpver/fedora-$fedoraver/{i386,SRPMS,x86_64}
+    mv /tmp/cp-fedora-$fedoraver-x86_64/*src.rpm /tmp/candlepin/$cpver/fedora-$fedoraver/SRPMS/
+    cp /tmp/cp-fedora-$fedoraver-x86_64/* /tmp/candlepin/$cpver/fedora-$fedoraver/i386/
+    cp /tmp/cp-fedora-$fedoraver-x86_64/* /tmp/candlepin/$cpver/fedora-$fedoraver/x86_64/
+    cd /tmp/candlepin/$cpver/fedora-$fedoraver/
+    create_repo fedora i386
+    create_repo fedora x86_64
+    create_repo fedora SRPMS
+}
+
+rm -rf /tmp/candlepin/$1/
+
+prep_epel_repo $1 5
+prep_epel_repo $1 6
+prep_fedora_repo $1 13
+prep_fedora_repo $1 14
+prep_fedora_repo $1 15
+
+#rsync -avz --delete --no-p --no-g /tmp/candlepin/$1/RHEL/ dept.rhndev.redhat.com:/var/www/dept/yum/candlepin/$1/RHEL/
